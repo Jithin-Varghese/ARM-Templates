337b10887619b322ae60cbc22b9c4a4a041c86d2
Jithin Varghese <jithinvarghese.varghese8@gmail.com>
2017-06-27 18:47:42 +0530

    Update script.sh

--- linux-vm/script.sh
+++ linux-vm/script.sh
@@ -1,5 +1,4 @@
-adminPasswordencoded =${1}
-adminPassword=${2}
-echo adminPasswordencoded $adminPasswordencoded >> /tmp/inputlog.txt
+adminPassword=${1}
+#echo adminPasswordencoded $adminPasswordencoded >> /tmp/inputlog.txt
 echo adminPassword $adminPassword >> /tmp/inputlog.txt
-echo "Testing"
+
