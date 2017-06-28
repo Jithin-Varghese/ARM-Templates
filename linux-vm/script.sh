f07155c818dcfbc06978df620e8ca6a05bdd4dc0
Jithin Varghese <jithinvarghese.varghese8@gmail.com>
2017-06-28 09:17:11 +0530

    Update script.sh

--- linux-vm/script.sh
+++ linux-vm/script.sh
@@ -1,4 +1,4 @@
-adminPassword=${1}
+adminPassword=${1} | base64 --decode 
 #echo adminPasswordencoded $adminPasswordencoded >> /tmp/inputlog.txt
 echo adminPassword $adminPassword >> /tmp/inputlog.txt
 
